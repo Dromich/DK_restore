af31dda42965514b6a0f0f497e07e20491ea07c9
Romich <hrushch.rr57@gmail.com>
2019-03-14 18:53:10 +0200

    buy form just greatet

--- VERSTKA/js/common.js
+++ VERSTKA/js/common.js
@@ -1,23 +1,23 @@
-$(function() {
+$(function () {
 
 	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
+	if (!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function () {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
+	$(".ajax_form").submit(function () { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
-		}).done(function() {
+		}).done(function () {
 			alert("Thank you!");
-			setTimeout(function() {
+			setTimeout(function () {
 				// Done Functions
 				th.trigger("reset");
 			}, 1000);
@@ -28,30 +28,32 @@ $(function() {
 	//Chrome Smooth Scroll
 	try {
 		$.browserSelector();
-		if($("html").hasClass("chrome")) {
+		if ($("html").hasClass("chrome")) {
 			$.smoothScroll();
 		}
-	} catch(err) {
+	} catch (err) {
 
 	};
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
+	$("img, a").on("dragstart", function (event) {
+		event.preventDefault();
+	});
+
 });
 
 $(document).ready(function () {
-		$(".preview").mousemove(function(e){
-		X = Math.floor(e.pageX-$(this).offset().left) +1 ;
-		Y = Math.floor(e.pageY-$(this).offset().top);
+	$(".preview").mousemove(function (e) {
+		X = Math.floor(e.pageX - $(this).offset().left) + 1;
+		Y = Math.floor(e.pageY - $(this).offset().top);
 		//$(this).text(X);
 		var page_a = 500 - X;
-		if(X < 0)
+		if (X < 0)
 			X = 0;
-		if(X > 500)
+		if (X > 500)
 			X = 500;
-		if(Y < 0)
+		if (Y < 0)
 			Y = 0;
-		if(Y > 330)
+		if (Y > 330)
 			Y = 330;
 		$('.car_b').css('width', X);
 		$('.preview .line').css('left', X);
@@ -59,13 +61,77 @@ $(document).ready(function () {
 		$('.line .mark').css('top', Y);
 	});
 
-
-
+	
+    
 
 
 });
 
-$(window).load(function() {
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+
+
+	$("#cities").load( "file.php" );
+
+    // get warehouses
+    $('#cities').change(function(){
+        var wh = $(this).val();
+
+        $.ajax({
+            url : 'file.php',
+            type : 'POST',
+            data : {
+                'warehouses' : wh,
+            },
+            success : function(data) {
+                $('#warehouses').html(data);
+            },
+            error : function(request,error)
+            {
+                $('#warehouses').html('<option>-</option>');
+            }
+        });
+    })
+
+	$('#olx_btn').click(function (e) { 
+		e.preventDefault();
+
+setTimeout(() => {
+	window.location.href=button.data('olx-link');
+	console.log(button.data('olx-link'))
+}, 800);
+
+		
+	});
+
+	$('#np_btn').click(function (e) { 
+		e.preventDefault();
+
+		$('#np_selectors').removeClass('hidden ');
+		
+	});
+
+$('.sb_btn').click(function (e) { 
+	e.preventDefault();
+	
+});
+
+
+	var button = $(event.relatedTarget) // Button that triggered the modal
+
+	var recipient = button.data('whatever') // Extract info from data-* attributes
+	// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	var modal = $(this)
+	modal.find('.modal-title').text('Замовлення товару ' + recipient)
+	modal.find('#goods_name').val(recipient)
+  })
+
+
+
+
+
+$(window).load(function () {
 
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
